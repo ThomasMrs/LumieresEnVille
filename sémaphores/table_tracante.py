b6b5b0f430bbeea659012b2fda2ed320261a02ea
fimbunamisu-create <180823596+MarcccSGT@users.noreply.github.com>
2026-06-17 09:52:53 +0200

    bonus

--- sémaphores/table_tracante.py
+++ sémaphores/table_tracante.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import math
 
 class SimulateurTable:
-    def __init__(self, root, fichier_csv):
+    def __init__(self, root, fichier_csv, couleur=(0, 0, 0)):
         self.top = tk.Toplevel(root)
         self.top.title("Simulateur Table Traçante")
         self.top.configure(bg="#333")
@@ -11,6 +11,9 @@ class SimulateurTable:
         self.H = 300
         self.CX = self.W / 2
         self.CY = self.H / 2
+        
+        # On convertit le tuple RGB (ex: 255, 0, 0) en code Hexadécimal (ex: #ff0000)
+        self.couleur_hex = f"#{couleur[0]:02x}{couleur[1]:02x}{couleur[2]:02x}"
         
         self.canvas = tk.Canvas(self.top, width=self.W, height=self.H, bg="white", highlightthickness=0)
         self.canvas.pack(padx=20, pady=20)
@@ -75,7 +78,7 @@ class SimulateurTable:
             self.canvas.itemconfig(self.stylo_visuel, fill="red")
             if self.derniere_pos:
                 px, py = self.derniere_pos
-                self.canvas.create_line(px, py, x, y, fill="black", width=2, capstyle=tk.ROUND, joinstyle=tk.ROUND)
+                self.canvas.create_line(px, py, x, y, fill=self.couleur_hex, width=2, capstyle=tk.ROUND, joinstyle=tk.ROUND)
         else:
             self.canvas.itemconfig(self.stylo_visuel, fill="lightblue")
             
@@ -83,7 +86,7 @@ class SimulateurTable:
         self.index_actuel += 1
         self.top.after(10, self.animer)
 
-def simuler_table_tracante_csv(fichier_csv, root_parent, duree_sec=10):
-    app = SimulateurTable(root_parent, fichier_csv)
+def simuler_table_tracante_csv(fichier_csv, root_parent, couleur=(0, 0, 0), duree_sec=10):
+    app = SimulateurTable(root_parent, fichier_csv, couleur)
     app.top.after(duree_sec * 1000, app.top.destroy)
     app.top.wait_window(app.top)
